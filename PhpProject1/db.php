3d16c468611a68ecd9946efd4642ed46dd720b2e
raghad <raghadmusfer5@gmail.com>
2026-03-10 02:02:23 +0300

    testing db

--- PhpProject1/db.php
+++ PhpProject1/db.php
@@ -4,9 +4,9 @@ $host = "localhost";
 $user = "root";
 $password = "root";
 $database = "it329_recipes";
-$port="8889";
+$port=8889;
 
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host, $user, $password, $database, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
