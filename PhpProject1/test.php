4ed9470700e71644814dd4ddaa9cff60ccdd76c2
dhaykhalid <dhaikhalid4@gmail.com>
2026-03-16 05:49:51 +0300

    حذفت كلاس التست

--- PhpProject1/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-//this class is only for checking the connection --- we r gonna delete it before submission!!
-include 'db.php';
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-echo "Database connected ";
-?>
